b4f471c55e6450401b0d385677009c5439090538
Darren Metoyer <darrenmetoyer@gmail.com>
2023-07-24 14:58:00 -0500

    Answer TODOs

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,33 @@
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+
+app.get("/authenticate", auth, (req, res) => {
+  console.log(`user logging in: ${req.auth.user}`);
+  res.cookie('user', req.auth.user, { signed: true });
+  res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  const upsertSucceeded = upsertUser(username, password)
+  res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie('user');
+  res.end();
+});
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+
 const express = require("express"),
        app = express(),
        port = process.env.PORT || 8080,
